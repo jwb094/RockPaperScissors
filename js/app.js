997302a8150a84083bbc154f4e48fb776e34d396
Tech-A33 <Tech-A33@tech-a33s-air.home>
2017-01-04 22:41:52 +0000

    added relevant comment for the ES6 structure

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-$(() => window.rps = new RPS());
+// when the window load it will create a new instance of RPS
+$(() => window.onload = new RPS());
 
 class RPS {
     constructor() {
@@ -14,46 +15,51 @@ class RPS {
         this.playerOption;
         this.computerOption;
 
-        //global variable for computerOption / playerOption / the result
+        //store the matrix array position
         this.result;
 
-        // the result message element from the front end
+        // the PLayer & Computer score count | The resultmessage element
         this.message = $("#resultmessage");
         this.playerScore = $("#playerCount");
         this.computerScore = $("#computerCount");
+
+        // the PLayer & Computer score count 
         this.playerScoreCount=0;
         this.computerScoreCount=0;
+        //the div element containing the images
         this.theButtons = $('.move-buttons div');
-        //console.log(this.theButtons);
 
-        // Run the object prototype function to add event listeners to the DOM elements above
+        // function for the button event listeners from the front end
         this.ButtonEventListener();
     }
     //A function that find a id from the clicked  button in the group of button in  .move-buttons div
     ButtonEventListener() {
         this.theButtons.each((i, button) => $(button).click(this.gameflow.bind(this)));
     }
-
+    //the game function flow how the game is played
     gameflow() {
         this.getPositions();
         this.getFinalMatrixPosition();
         this.outcome();
         this.ScoreUpdate();
-
     }
+    //this function retrieve teh player and computer choice
     getPositions() {
         this.playerOption = event.target.id;
         this.computerOption = [Math.floor(Math.random() * this.whoWonTheBattle.length)];
     }
+    //the Matrix positionis found using the player and computer choices
     getFinalMatrixPosition(){
         this.result = this.whoWonTheBattle[this.playerOption][this.computerOption];
     }
+    //the winner is found
     outcome(){
         this.theOutcome = this.result; 
         this.theOutcome === "P" ? $(this.message).html("Player wins"): 
         this.theOutcome === "C" ? $(this.message).html("Computer wins") : 
             $(this.message).html("Its a draw");   
     }
+    //the winner's score is update
     ScoreUpdate(){
         this.winner = this.result;
          this.winner === "P" ? this.playerScore.html(this.playerScoreCount++):
