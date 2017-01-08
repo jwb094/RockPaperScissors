e9fbb45b4f7b9b2f8a20d61ed7eff53d4163f69a
Tech-A33 <Tech-A33@tech-a33s-air.home>
2017-01-08 21:21:17 +0000

    added somecode

--- js/app.js
+++ js/app.js
@@ -66,4 +66,83 @@ class RPS {
          $(this.message).html("Its a draw");   
 
     }
-}
+}/*
+-document.addEventListener('DOMContentLoaded' , function(){
+ +$(() => window.rps = new RockPaperScissors());
+  
+ -//Matrix array - the possible result from ROCK PAPER SCISSORS  for Player and COmputer ; Row then Column
+ -let whoWonTheBattle = [
+ +
+ +class RPS {
+ +    constructor(){
+ +        //Matrix array - the possible result from ROCK PAPER SCISSORS  for Player and COmputer ; Row then Column
+ +        this.whoWonTheBattle = [
+               //(C)Rock Paper Scissors
+ -/*(P)rock */ ['D','C','P'],
+ -/*Paper */   ['P','D','C'],
+ -/*Scissors*/ ['C','P','D']
+ -];
+ +            /*(P)rock */ ['D','C','P'],
+ +            /*Paper */   ['P','D','C'],
+ +            /*Scissors*/ ['C','P','D']
+ +        ];
+ +        
+ +        //global variable for computerOption / playerOption / the result
+ +        this.playerOption;
+ +        this.computerOption;
+ +
+ +        // the result message element from the front end
+ +        this.message = $("#resultmessage");
+ +
+ +        this.theButtons = $('.move-buttons div');
+ +
+ +    }
+ +
+ +
+ +}
+ +
+  
+  //Player score and computer score which starts at zero
+  let playerScoreCount = 0;
+  let computerScoreCount = 0;
+  
+  //global variable for computerOption / playerOption / the result
+ -let computerOption;
+ -let playerOption;
+  let result;
+ -let theButtons = $('.move-buttons div');
+ +
+  
+      //A function that find a id from the clicked  button in the group of button in  .move-buttons div
+      theButtons.each(function (i, button){
+ @@ -35,27 +51,28 @@
+ 
+     // A function that calculate the final position in the Matrix 
+     function getFinalPosition(){
+         let result = whoWonTheBattle[PlayerMove][ComputerMove];
+         TheWinner(result);
+     }
+ 
+     //A function that perform actions if the Player wins
+     function PlayerOutput(){
+         $("#resultmessage" ).html("Player wins");
+         playerScoreCount++;
+         let theScore = document.getElementById("Playercount").innerHTML = playerScoreCount;
+     }
+     //A function that perform actions if the Computer wins
+     function ComputerOutput(){
+         $("#resultmessage").html("Computer wins");
+         computerScoreCount++;
+         let theScore = document.getElementById("Computercount").innerHTML = computerScoreCount;
+     }
+ 
+      // A function that calculate the Winner by the position in the matrix
+      function TheWinner (theOutcome){
+              //if Outcome = "P" Player wins    if Outcome = "C" Computer wins          
+             this.theOutcome = result; theOutcome === "P" ? PlayerOutput(): theOutcome === "C" ? ComputerOutput():
+             this.theOutcome = result; 
+             theOutcome === "P" ? PlayerOutput(): theOutcome === "C" ? ComputerOutput():
+              $("#resultmessage" ).text("It's a draw!");    
+          }
+  }); 
+ */
